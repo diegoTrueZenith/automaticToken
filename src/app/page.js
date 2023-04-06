f5a2be0150d2fa0767cf7e01c763aaedd7822796
diegoTrueZenit <diego@truezenithcreative.com>
2023-04-06 16:38:27 -0700

    firstCommit

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,72 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { doc, collection, addDoc, updateDoc } from "firebase/firestore"; 
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
+
+//   console.log(" ============ HERE IS THE RESULT ============");
+//   fetch('https://auth.hospitable.com/oauth/token', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     client_id: 'b115db6a-cc55-4b3b-8d27-b7183318a9ce',
+//     client_secret: '23835ee9f72aa85379da7abcef5e3b7eaee941e46558a4648152ae2a63cb87287',
+//     audience: 'api.hospitable.com',
+//     grant_type: 'client_credentials'
+//   })
+// })
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error(error));
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvv6zU1DNGi1LoidkOAu3Ro2JLcrplf0U",
+  authDomain: "truezenithcreative-85781.firebaseapp.com",
+  projectId: "truezenithcreative-85781",
+  storageBucket: "truezenithcreative-85781.appspot.com",
+  messagingSenderId: "199949387061",
+  appId: "1:199949387061:web:c94b1a2fab756448b5f87c"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const currentTime = new Date().toLocaleTimeString();
+
+const currentDate = new Date().toLocaleDateString('en-US', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit'
+});
+
+
+ async function addData(){
+
+
+const tokenRef = doc(db, "360HomeFinder", "token");
+await updateDoc(tokenRef, {
+  date: currentDate,
+  time: currentTime,  
+  token: "HERE GOES YOUR TOKEN"
+});
+ }
+
+ addData();
+
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
