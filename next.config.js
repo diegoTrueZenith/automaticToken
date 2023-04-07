9872145d837b2dabcb2303b450e725d8b2264dc9
diegoTrueZenit <diego@truezenithcreative.com>
2023-04-07 11:12:10 -0700

    .

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
+            value: 'public, max-age=3600, must-revalidate',
           },
         ],
       }
