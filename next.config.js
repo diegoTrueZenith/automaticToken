b37022cfc6a4a9487230baf4806106c428db70e1
diegoTrueZenit <diego@truezenithcreative.com>
2023-04-07 11:04:51 -0700

    .

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=3600',
+          },
+        ],
+      }
+    ]
+  },
+
   experimental: {
     appDir: true,
-  },
+  }
 }
 
 module.exports = nextConfig
